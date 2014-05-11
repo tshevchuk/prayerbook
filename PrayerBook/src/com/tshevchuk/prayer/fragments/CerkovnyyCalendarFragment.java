04b02ba34725fb6eaa34e824155fd1928cc2722e
Taras Shevchuk <taras.shevchuk@gmail.com>
2014-05-12 00:28:43 +0300

    Cerkovnyy Calendar partially implemented

--- PrayerBook/src/com/tshevchuk/prayer/fragments/CerkovnyyCalendarFragment.java
+++ PrayerBook/src/com/tshevchuk/prayer/fragments/CerkovnyyCalendarFragment.java
@@ -2,6 +2,7 @@ package com.tshevchuk.prayer.fragments;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +32,7 @@ public class CerkovnyyCalendarFragment extends FragmentBase {
 		TextView tvCalendar = (TextView) v.findViewById(R.id.tv_calendar);
 		StringBuilder sb = new StringBuilder();
 		java.util.Calendar cal = java.util.Calendar.getInstance();
-		cal.set(2014, 0, 1);
+		cal.set(2014, 6, 1);
 		CerkovnyyCalendar cc = CerkovnyyCalendar.getInstance();
 		while(cal.get(java.util.Calendar.YEAR) < 2015){
 			sb.append(cc.getCalendarDay(cal.getTime())).append("\n\n");
