ea06bd99555f150e9943c7ee5c9e62f6f6fb13dd
Taras Shevchuk <taras.shevchuk@gmail.com>
2014-06-07 09:32:29 +0300

    Recent Items partially implemented

--- PrayerBook/src/com/tshevchuk/prayer/PreferenceManager.java
+++ PrayerBook/src/com/tshevchuk/prayer/PreferenceManager.java
@@ -1,14 +1,21 @@
 package com.tshevchuk.prayer;
 
+import java.util.Arrays;
+
 import android.content.SharedPreferences;
+import android.text.TextUtils;
 
 public class PreferenceManager {
 	public static final String PREF_TEXT_FONT_SIZE = "pref_textFontSize";
 	public static final String PREF_NIGHT_MODE = "pref_nightMode";
+	public static final String PREF_RECENT_MENU_ITEMS = "pref_recentMenuItems";
+
+	private static final int MAX_RECENT_ITEMS_COUNT = 30;
 
 	private static PreferenceManager instance = new PreferenceManager();
 
 	private SharedPreferences sharedPrefs;
+	private int[] recentMenuItemsId;
 
 	private PreferenceManager() {
 		sharedPrefs = android.preference.PreferenceManager
@@ -27,5 +34,60 @@ public class PreferenceManager {
 	public int getFontSizeSp() {
 		return Integer.parseInt(sharedPrefs
 				.getString(PREF_TEXT_FONT_SIZE, "18"));
+	}
+
+	public int[] getRecentMenuItems() {
+		if (recentMenuItemsId == null) {
+			String s = sharedPrefs.getString(PREF_RECENT_MENU_ITEMS, "");
+			String[] idsStr = TextUtils.split(s, "|");
+			int[] ids = new int[idsStr.length];
+			for (int i = 0; i < idsStr.length; ++i) {
+				ids[i] = Integer.parseInt(idsStr[i]);
+			}
+			recentMenuItemsId = ids;
+		}
+		return recentMenuItemsId;
+	}
+
+	public void markMenuItemAsOpened(int menuItemId) {
+		int[] recentIds = getRecentMenuItems();
+		int index = -1;
+		for (int i = 0; i < recentIds.length; ++i) {
+			if (recentIds[i] == menuItemId) {
+				index = i;
+				break;
+			}
+		}
+		int midIndex = MAX_RECENT_ITEMS_COUNT / 2;
+		int newIndex;
+		if (index >= 0 && index <= midIndex) {
+			newIndex = Math.max(0, index - 1);
+		} else {
+			newIndex = Math.min(midIndex, recentIds.length);
+		}
+		int newSize = index == -1 ? Math.min(MAX_RECENT_ITEMS_COUNT,
+				recentIds.length + 1) : recentIds.length;
+
+		int[] newRecentIds = new int[newSize];
+		for (int i = 0; i < newRecentIds.length; ++i) {
+			if (i < newIndex) {
+				newRecentIds[i] = recentIds[i];
+			} else if (i == newIndex) {
+				newRecentIds[i] = menuItemId;
+			} else { // i > newIndex
+				if (index == -1 || (i <= index)) {
+					newRecentIds[i] = recentIds[i - 1];
+				} else {
+					newRecentIds[i] = recentIds[i];
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder(1000);
+		sb.append(newRecentIds[0]);
+		for (int i = 1; i < newRecentIds.length; ++i) {
+			sb.append('|').append(newRecentIds[i]);
+		}
+		recentMenuItemsId = newRecentIds;
+		sharedPrefs.edit().putString(PREF_RECENT_MENU_ITEMS, sb.toString());
 	}
 }
