f44ecd9c15ae5120fcf506af125d6765ccb4bf22
Taras Shevchuk <taras.shevchuk@gmail.com>
2014-08-12 00:10:29 +0300

    Add texts to Catholic PrayerBook

--- PrayerBookCatholic/src/com/tshevchuk/prayer/data/Catalog.java
+++ PrayerBookCatholic/src/com/tshevchuk/prayer/data/Catalog.java
@@ -1,22 +1,28 @@
 package com.tshevchuk.prayer.data;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.content.res.XmlResourceParser;
 import android.text.TextUtils;
 import android.util.SparseArray;
 
 import com.tshevchuk.prayer.PrayerBookApplication;
 import com.tshevchuk.prayer.Utils;
+import com.tshevchuk.prayer.catholic.R;
 
 public class Catalog {
 	public static final int ID_CALENDAR = 1;
 	public static final int ID_RECENT_SCREENS = 2;
 
-	private static final int NEXT_ID_TO_ADD = 39;
+	private static final int NEXT_ID_TO_ADD = 58;
 
 	private List<MenuItemBase> topMenu = new ArrayList<MenuItemBase>();
 	private SparseArray<MenuItemBase> menuItemsByIds = new SparseArray<MenuItemBase>();
@@ -26,59 +32,56 @@ public class Catalog {
 		menu.addSubItem(new MenuItemOftenUsed(ID_RECENT_SCREENS));
 		menu.addSubItem(new MenuItemCalendar(ID_CALENDAR));
 
-		MenuItemSubMenu sm = menu.subMenu(3, "1. Молімося кожного дня");
-		sm.web(4, "1.1. Щоденні молитви", "prayers/schodenni-molytvy.html",
-				null);
-		sm.web(5, "1.2. Катихізис Католицької Церкви",
-				"prayers/katehyzis.html", null);
-		sm.subMenu(6, "1.3. Ранкові молитви");
-		sm.subMenu(7, "1.4. Молитви протягом дня");
-		sm.subMenu(8, "1.5. Вечірні молитви");
-		sm.subMenu(9, "1.6. Молитва сім’ї");
-		sm.subMenu(10, "1.7. Молитва псалмами");
-		sm.subMenu(11, "1.8. Роздуми Lectio Divina");
-
-		sm = menu.subMenu(12, "2. Таїнства Христа і Церкви");
-		sm.subMenu(13, "2.1. Таїнство Хрещення");
-		sm.subMenu(14, "2.2. Таїнство миропомазання");
-		sm.subMenu(15, "2.3. Таїнство Євхаристії");
-		sm.subMenu(16, "2.3.1. Свята Літургія римського обряду");
-		sm.subMenu(17, "2.3.2. Подяка після Святої Меси");
-		sm.subMenu(18, "2.4. Таїнство Покаяння і Примирення");
-		sm.subMenu(19, "2.5. Таїнство Єлеопомазання");
-		sm.subMenu(20, "2.6. Таїнство Священства");
-		sm.subMenu(21, "2.7. Таїнство Подружжя");
-
-		sm = menu.subMenu(22, "3. Молитви на честь Триєдиного Бога");
-		sm.subMenu(23, "3.1. До Бога Отця");
-		sm.subMenu(24, "3.2. До Бога Сина");
-		sm.subMenu(25, "3.3. На честь Святого Духа");
-		sm.subMenu(26, "3.4. На честь Божого милосердя");
-
-		sm = menu.subMenu(27, "4. Молитви до Ангелів");
-		sm.subMenu(28, "4.1. Молитви під час боротьби з силами темряви");
-
-		menu.subMenu(29, "5. Молитви на честь Богородиці");
-
-		sm = menu.subMenu(30, "6. Молитви на честь святих і про їх заступництво");
-		sm.subMenu(31, "6.1. На честь святого Йосифа");
-		sm.subMenu(32, "6.2. Молитви до різних святих");
-
-		menu.subMenu(33, "7. Молитви за хворих");
-
-		menu.subMenu(34, "8. Молитви за помираючих");
-
-		menu.subMenu(35, "9. Молитви за померлих");
-
-		menu.subMenu(36, "10. Молитви в різних потребах");
-
-		menu.subMenu(37, "11. Церковний рік");
-
-		menu.subMenu(38, "12. Молитви латинською мовою");
+		try {
+			readFromXml(menu);
+		} catch (XmlPullParserException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		topMenu = menu.getSubItems();
 
 		verifyUniqueId();
+	}
+
+	private void readFromXml(MenuItemSubMenu topMenu)
+			throws XmlPullParserException, IOException {
+		XmlResourceParser xrp = PrayerBookApplication.getInstance()
+				.getResources().getXml(R.xml.catalog);
+		xrp.next();
+		int eventType = xrp.getEventType();
+		int menuLevel = 0;
+		ArrayList<MenuItemSubMenu> subItems = new ArrayList<MenuItemSubMenu>();
+		subItems.add(topMenu);
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			if (eventType == XmlPullParser.START_DOCUMENT) {
+			} else if (eventType == XmlPullParser.START_TAG) {
+				String tag = xrp.getName();
+				int id = xrp.getAttributeIntValue(null, "id", 1);
+				String name = xrp.getAttributeValue(null, "name");
+				String file = "prayers/" + xrp.getAttributeValue(null, "file");
+				if (tag.equals("submenu")) {
+					MenuItemSubMenu mi = subItems.get(menuLevel).subMenu(id,
+							name);
+					if (subItems.size() > menuLevel + 1) {
+						subItems.set(menuLevel + 1, mi);
+					} else {
+						subItems.add(mi);
+					}
+					menuLevel++;
+				} else if (tag.equals("html")) {
+					subItems.get(menuLevel).web(id, name, file, null);
+				}
+			} else if (eventType == XmlPullParser.END_TAG) {
+				String tag = xrp.getName();
+				if (tag.equals("submenu")) {
+					menuLevel--;
+				}
+			} else if (eventType == XmlPullParser.TEXT) {
+			}
+			eventType = xrp.next();
+		}
 	}
 
 	public List<MenuItemBase> getTopMenuItems() {
